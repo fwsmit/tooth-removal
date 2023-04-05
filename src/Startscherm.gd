8319a91311c52a7ffd3f1adff6e0017a6581f58d
Friso Smit <fw.smit01@gmail.com>
2023-04-05 16:26:42 +0200

    Voeg startscherm toe met student/chirurg selectie

---
+++ src/Startscherm.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@export var list: ItemList
+@export var loginButton: Button
+
+
+func login():
+	print("Loggin in as: ", list.get_item_text(list.get_selected_items()[0]))
+	get_tree().change_scene_to_file("res://main.tscn")
+
+func _ready():
+	list.select(0)
+	loginButton.connect("pressed", login)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
