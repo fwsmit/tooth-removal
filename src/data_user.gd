1b36abfa7cd2d63cc1c10e409bc8765e07e20f14
Friso Smit <fw.smit01@gmail.com>
2023-04-24 15:10:27 +0200

    Clean up data_user.gd

--- src/data_user.gd
+++ src/data_user.gd
@@ -28,18 +28,12 @@ func _handle_client_connected() -> void:
 	print("Client connected to server.")
 
 func convert_torque(torque, force, location):
-	#print("Force cross: ", location.cross(force))
-	#print("Torque: ", torque)
 	var result = torque - location.cross(force)
 	return result
 	
 
 var i = 0;
 func _handle_client_data(force, torque) -> void:
-	#print(Fx, Fy, Fz, Tx, Ty, Tz)
-#	uint64_t *p64 = (uint64_t *)buf;
-#	*p64 = BSWAP64(*p64);
-#	var Fx_d = encode_double(Fx)
 	var kaak_locatie_onderkaak = Vector3(0.231, 0.062, 0)
 	var tand_offset = Vector3(-0.01,0.02,0)
 	var tand_locatie = kaak_locatie_onderkaak + tand_offset
@@ -47,24 +41,7 @@ func _handle_client_data(force, torque) -> void:
 	# Convert to numbers around 1
 	force = force / 40
 	torque = torque / 3
-#	Fx = Fx/40
-#	Fy = Fy/40
-#	Fz = Fz/40
-#	Tx = Tx/3
-#	Ty = Ty/3
-#	Tz = Tz/3
-#	if i > 10: 
-#		print(Fx[1])
-#		var reversed = reverse_bytearray(Fx[1])
-#		print(reversed)
-#		print(reversed.decode_double(0))
-#		print(Fx)
-#		print(Fy)
-#		i = 0
-#	i += 1
-	#if (Fx > 1):
-		#print(Fx)
-#	print(Fx, ", ", Fy)
+#	
 	transform.origin.x = force.x
 	transform.origin.y = force.y
 	transform.origin.z = force.z
@@ -73,8 +50,6 @@ func _handle_client_data(force, torque) -> void:
 	rotation = rotation.rotated(Vector3( 0, 1, 0 ), torque.y)
 	rotation = rotation.rotated(Vector3( 0, 0, 1 ), torque.z)
 	transform.basis = rotation.basis
-	#var message: PackedByteArray = [97, 99, 107] # Bytes for "ack" in ASCII
-	#_client.send(message)
 
 func _handle_client_disconnected() -> void:
 	emit_signal("disconnected")
