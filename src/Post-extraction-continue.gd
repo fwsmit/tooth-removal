9c346308a46138a5b093003b2c3afdd069b027ae
Friso Smit <fw.smit01@gmail.com>
2023-04-25 11:07:07 +0200

    Begin on implementing json conversion

--- src/Post-extraction-continue.gd
+++ src/Post-extraction-continue.gd
@@ -1,6 +1,14 @@
 extends Button
 
+func save_extraction_to_file(filename):
+	var extraction_data = {
+		"quadrant": Global.selectedQuadrant,
+		"tooth": Global.selectedTooth,
+		"type": Global.selectedType,
+	}
+	print(JSON.stringify(extraction_data, "\t"))
 
 # Called when the node enters the scene tree for the first time.
 func _pressed():
+	save_extraction_to_file("test.json")
 	Global.goto_scene("res://dashboard.tscn")
