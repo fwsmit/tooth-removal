82cfdcd33054fdafc5f519f36cbecab733f3a1ce
Friso Smit <fw.smit01@gmail.com>
2023-05-02 18:05:13 +0200

    Use checkboxes for filtering

--- src/scripts/Previous-extractions/ExtractionList.gd
+++ src/scripts/Previous-extractions/ExtractionList.gd
@@ -1,8 +1,19 @@
 extends ItemList
 
-@export var extractionList : ItemList
-@export var filterOptions : OptionButton
+@export var quadrantFilter : GridContainer
+@export var toothFilter : GridContainer
+@export var personFilter : GridContainer
+
+var filters
 var index
+
+func checkChildButtons(node):
+	for child in node.get_children():
+		child.button_pressed = true
+
+func connectUpdate(node):
+	for child in node.get_children():
+		child.connect("pressed", updateFilter)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,50 +25,33 @@ func _ready():
 	var filepath = FileAccess.open("user://index.json", FileAccess.READ)
 	index = JSON.parse_string(filepath.get_as_text())
 	for filename in index:
-		extractionList.add_item(filename)
+		add_item(filename)
 	
-	for q in range(4):
-		for t in range(8):
-			filterOptions.add_item("Quadrant " + str(q+1) + ", tooth "+ str(t+1), (q+1)*10+t+1)
+	filters = [quadrantFilter, toothFilter, personFilter]
+	for f in filters:
+		checkChildButtons(f)
+		connectUpdate(f)
 	
 	Global.selectedFile = null
 	connect("item_selected", itemSelected)
-	
-func itemSelected(index):
-	Global.selectedFile = get_item_text(index)
 
-func addUpperJawFiles(list):
-	for f in index:
-		if index[f]["quadrant"] == 1 or index[f]["quadrant"] == 2:
-			list.add_item(f)
+func itemSelected(i):
+	Global.selectedFile = get_item_text(i)
 
-func addLowerJawFiles(list):
-	for f in index:
-		if index[f]["quadrant"] == 3 or index[f]["quadrant"] == 4:
-			list.add_item(f)
+func n_th_button_checked(filter, n):
+	return filter.get_children()[n].button_pressed
 
-func addTeethFiles(list, q, t):
-	for f in index:
-		if index[f]["quadrant"] == q and index[f]["tooth"] == t:
-			list.add_item(f)
+func filter_quadrant(filename):
+	var q = index[filename]["quadrant"]
+	return n_th_button_checked(quadrantFilter, q-1)
 
-func filterSelected(filter):
-	print("Filter selected: ", filter)
-	extractionList.clear()
-	if filter == 0:
-		for f in index:
-			extractionList.add_item(f)
-		return
-	if filter == 1:
-		extractionList.clear()
-		addUpperJawFiles(extractionList)
-		return
-	if filter == 2:
-		addLowerJawFiles(extractionList)
-		return
-	var id = filterOptions.get_item_id(filter)
-	var q = floor(float(id)/10)
-	var t = id % 10
-	print("Quadrant "+str(q)+", tooth ", str(t))
-	addTeethFiles(extractionList, q, t)
-	pass
+func filter_tooth(filename):
+	var t = index[filename]["tooth"]
+	return n_th_button_checked(toothFilter, t-1)
+
+func updateFilter():
+	print("Updating filter")
+	clear()
+	for filename in index:
+		if filter_quadrant(filename) and filter_tooth(filename):
+			add_item(filename)
