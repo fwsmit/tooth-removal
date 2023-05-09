bc7fb4213ef3105e909ef02c51116bdce47efa31
bmkroon <131759151+bmkroon@users.noreply.github.com>
2023-05-09 14:10:49 +0200

    Test of commit werkt > hoeft niet te mergen

---
+++ src/scenes/Roation_node.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if len(Global.clinical_directions) > 0:
+		print(Global.clinical_directions[1])
