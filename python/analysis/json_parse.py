40fc2fcf832ba483b23a3d5ce179bd020e4bf3e0
Friso Smit <fw.smit01@gmail.com>
2023-05-09 12:49:50 +0200

    Fix index not being created when no file exists

--- python/analysis/json_parse.py
+++ python/analysis/json_parse.py
@@ -3,5 +3,7 @@ import os
 
 def parse_json(filename, dataDir):
     filepath = os.path.join(dataDir, filename)
+    if not os.path.isfile(filepath):
+        return {}
     with open(filepath) as f:
         return json.load(f)
