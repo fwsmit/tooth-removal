42cd17c085f801b18749ff506fe2967331dcb64b
bmkroon <131759151+bmkroon@users.noreply.github.com>
2023-05-10 12:03:34 +0200

    Renamed and deleted Extraction screen

--- src/scripts/Pre-extraction/Pre-extraction-continue.gd
+++ src/scripts/Pre-extraction/Pre-extraction-continue.gd
@@ -3,7 +3,7 @@ extends Button
 # Called when the node enters the scene tree for the first time.
 func _pressed():
 	if Global.is_pre_extraction_data_valid():
-		Global.goto_scene("res://scenes/extraction2.tscn")
+		Global.goto_scene("res://scenes/extraction.tscn")
 	else:
 		OS.alert("Please select the quadrant, tooth and type")
 		
