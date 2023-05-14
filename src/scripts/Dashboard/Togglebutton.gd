bf24f84984d670ffb2b69cf5b8fa37c5a6d9b47c
Friso Smit <fw.smit01@gmail.com>
2023-05-14 18:45:29 +0200

    Fix warning on toggle button

--- src/scripts/Dashboard/Togglebutton.gd
+++ src/scripts/Dashboard/Togglebutton.gd
@@ -18,8 +18,6 @@ func _on_gui_input(event):
 	print("input")
 	if event is InputEventMouseButton:
 		if event.is_pressed() and event.get_button_index() == 1:
-			$AnimationPlayer.play("Click")
-			
 			if sidebar.custom_minimum_size.x == 56:
 				sidebar.custom_minimum_size.x = 100
 			else:
