d04555ee230a845b73ffb3023b8c6de737090766
Friso Smit <fw.smit01@gmail.com>
2023-05-14 18:47:18 +0200

    Remove unused code

--- src/scripts/Dashboard/Togglebutton.gd
+++ src/scripts/Dashboard/Togglebutton.gd
@@ -1,18 +1,6 @@
 extends Panel
 
-#@export var SidebarPath : NodePath
-
 @export var sidebar: HBoxContainer
-
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-#	sidebar = get_node(SidebarPath)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_gui_input(event):
 	print("input")
@@ -22,7 +10,3 @@ func _on_gui_input(event):
 				sidebar.custom_minimum_size.x = 100
 			else:
 				sidebar.custom_minimum_size.x = 56
-
-
-func _on_csg_mesh_3d_disconnected():
-	pass # Replace with function body.
